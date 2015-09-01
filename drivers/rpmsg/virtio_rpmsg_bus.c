53e3146add82283465a3d1e5cd674fa17cf5e2a1
Ajo Jose Panoor <ajopanoor@gmail.com>
2015-09-01 13:18:21 +0200

    Temporary workaround for the Tx ring full issue. Though the Tx code has ways to metigate this issue, a missing virtio_mb before calling "more_used" is causing the host and phi not to see the last updated vring.used->idx send virtqueue (svq). This resulted in the tx full situation. Now it is mitigated by calling virtqueue_enable_cb. (Ideally a virtio_rmb(vq->weak_barriers) would have done the trick. Hmm..)

--- drivers/rpmsg/virtio_rpmsg_bus.c
+++ drivers/rpmsg/virtio_rpmsg_bus.c
@@ -748,11 +748,15 @@ static void release_tx_bufs(struct virtproc_info *vrp)
 	struct buf_info *tx_info;
 	unsigned int count = 0;
 	unsigned int len;
-
+retry:
 	while ((count < vrp->max_frees) &&
 			(tx_info = virtqueue_get_buf(vrp->svq, &len))) {
 		free_tx_buf(vrp, tx_info);
 		count++;
+	}
+	if (count == 0 && vrp->svq->num_free < vrp->max_frees) {
+		virtqueue_enable_cb(vrp->svq);
+		goto retry;
 	}
 	dev_info(&vrp->vdev->dev, "%s tx done num_free %d count %lu\n",__func__,
 						vrp->svq->num_free, count);
